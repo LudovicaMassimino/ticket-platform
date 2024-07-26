ba48825cdf01a67c5fc9f5263e3ae9380efede1b
LudovicaMassimino <ludovica.massimino93@gmail.com>
2024-07-26 17:27:00 +0200

    edite, delete e varie modifiche dettagli

--- ticket-platform/src/main/java/it/ludo/ticket_platform/controller/TicketController.java
+++ ticket-platform/src/main/java/it/ludo/ticket_platform/controller/TicketController.java
@@ -1,17 +1,23 @@
 package it.ludo.ticket_platform.controller;
+
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.ludo.ticket_platform.model.Ticket;
 import it.ludo.ticket_platform.repository.TicketRepo;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 @Controller
 @RequestMapping("/ticket")
@@ -49,4 +55,41 @@ public class TicketController {
 
         return "/common/info";
     }
+
+    @GetMapping("/{id}/edit")
+    public String showEditForm(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("ticket", ticketRepo.getReferenceById(id));
+        model.addAttribute("status", Ticket.Status.values());
+        return "/common/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String Update(@PathVariable("id") Integer id, @Valid @ModelAttribute("ticket") Ticket ticketUpdate,
+            BindingResult bindingresult,
+            Model model) {
+
+        if (bindingresult.hasErrors()) {
+
+            return "/common/edit";
+        }
+        Ticket existingTicket = ticketRepo.getReferenceById(id);
+
+        existingTicket.setTitle(ticketUpdate.getTitle());
+        existingTicket.setBody(ticketUpdate.getBody());
+        existingTicket.setStatus(ticketUpdate.getStatus());
+
+        ticketRepo.save(existingTicket);
+
+        return "redirect:/ticket/{id}";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Integer id) {
+        // TODO: process POST request
+
+        ticketRepo.deleteById(id);
+
+        return "redirect:/ticket/admin";
+    }
+
 }
--- ticket-platform/src/main/java/it/ludo/ticket_platform/model/Ticket.java
+++ ticket-platform/src/main/java/it/ludo/ticket_platform/model/Ticket.java
@@ -1,11 +1,14 @@
 package it.ludo.ticket_platform.model;
 
 import java.time.LocalDateTime;
+
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,36 +16,49 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "ticket")
+@Table(name = "TICKET")
 public class Ticket {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Campo Obbligatorio")
     @Column(nullable = false)
     private String title;
 
+    @NotBlank(message = "Campo Obbligatorio")
     @Column(nullable = false)
     private String body;
 
+    // @NotNull(message = "Campo Obbligatorio")
     @Column(nullable = false)
     private LocalDateTime ticket_date;
 
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        DA_FARE, IN_CORSO, COMPLETATO
+    }
+
+    // @NotNull(message = "Campo Obbligatorio")
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    // @NotNull(message = "Campo Obbligatorio")
     @ManyToOne
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
-    @OneToMany(mappedBy = "ticket" , cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
     private List<Note> note;
 
-    // getter e setter
+    // GETTERS & SETTERS
 
     public List<Note> getNote() {
         return note;
@@ -50,6 +66,22 @@ public class Ticket {
 
     public void setNote(List<Note> note) {
         this.note = note;
+    }
+
+    public LocalDateTime getTicket_date() {
+        return ticket_date;
+    }
+
+    public void setTicket_date(LocalDateTime ticket_date) {
+        this.ticket_date = ticket_date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public Integer getId() {
@@ -84,19 +116,11 @@ public class Ticket {
         this.user = user;
     }
 
-    public Category getCategory() {
-        return category;
+    public Status getStatus() {
+        return status;
     }
 
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public LocalDateTime getTicket_date() {
-        return ticket_date;
-    }
-
-    public void setTicket_date(LocalDateTime ticket_date) {
-        this.ticket_date = ticket_date;
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
